e0efec57f15c17c0c623647fb7521247f74cfd3a
Sara Jensen <jensen.sara.e@gmail.com>
2017-05-11 14:48:05 -0700

    Add delete and put routes to delete or update a note using its unique id in the database.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,10 @@
 // To create a note:
-// Make sure to grab ObjectID from mongodb, apparently. 
-const ObjectID = require('mongodb').ObjectID;
+// Make sure to grab ObjectID from mongodb, apparently.
+var ObjectID = require('mongodb').ObjectID;
 
 //in Express, routes are wrapped in a function, which takes the Express instance (app) and a database (db) as arguments. We export the function via index.js to server.js for use.
 module.exports = function(app, db) {
+
   // Get back the note we just created, using the note/:id route
   app.get('/notes/:id', (req, res) => {
     // Mongodb requires an ObjectID, but it's easy to create from a string, you can get the id from the db, then create a new ObjectID, like so.
@@ -18,13 +19,40 @@ module.exports = function(app, db) {
     });
   });
 
+  // Delete a note using its id
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id' : new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' has been deleted. You are welcome');
+      }
+    });
+  });
+
+  // Update a note using its id
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      }
+    });
+  });
+
   // your CREATE route goes here:
   // When the app receives a POST request to the /notes path, it will execute the code inside the callback - passing in a request object, which contains the parameters or JSON of the request, and a response object, used to reply.
   // req is request and res is response, the response here being 'Hello'. These must be built-in parameters from the Express library??
   // Test this in Postman
   app.post('/notes', (req, res) => {
     // Unfortunately, Express can’t process URL encoded forms on its own. But you did install that body-parser package… So ask express to use the bodyParser in the server.js file
-    const note = { text: req.body.body, title: req.body.title };
+    const note = { texto: req.body.body, titulo: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
