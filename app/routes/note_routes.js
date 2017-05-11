4d187480040696a5217b5c5cd4e4d71110918f7f
Sara Jensen <jensen.sara.e@gmail.com>
2017-05-11 14:25:12 -0700

    Get a note by making a get request with the notes/:id route, but you need the object id to make it work.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,10 +1,25 @@
-// To create a note, we're building more infrastructure:
+// To create a note:
+// Make sure to grab ObjectID from mongodb, apparently. 
+const ObjectID = require('mongodb').ObjectID;
 
 //in Express, routes are wrapped in a function, which takes the Express instance (app) and a database (db) as arguments. We export the function via index.js to server.js for use.
-
 module.exports = function(app, db) {
+  // Get back the note we just created, using the note/:id route
+  app.get('/notes/:id', (req, res) => {
+    // Mongodb requires an ObjectID, but it's easy to create from a string, you can get the id from the db, then create a new ObjectID, like so.
+    const id = req.params.id;
+    const details = { '_id' : new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   // your CREATE route goes here:
-  // When the app receives a POST request to the /notes path, it will execute the code inside the callback- passing in a request object, which contains the parameters or JSON of the request, and a response object, used to reply.
+  // When the app receives a POST request to the /notes path, it will execute the code inside the callback - passing in a request object, which contains the parameters or JSON of the request, and a response object, used to reply.
   // req is request and res is response, the response here being 'Hello'. These must be built-in parameters from the Express library??
   // Test this in Postman
   app.post('/notes', (req, res) => {
